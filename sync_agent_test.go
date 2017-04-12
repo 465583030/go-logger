b450c22feeb05e3b3533be580c9ce695657936d0
Will Charczuk <will@blendlabs.com>
2017-04-12 15:55:40 -0700

    test updates

--- sync_agent_test.go
+++ sync_agent_test.go
@@ -26,3 +26,17 @@ func TestSyncAgentInfof(t *testing.T) {
 	assert.Equal("this is a %s", format)
 	assert.Equal("info this is a test\n", buffer.String())
 }
+
+func TestSyncAgentDebugf(t *testing.T) {
+	assert := assert.New(t)
+
+	buffer := bytes.NewBuffer(nil)
+	a := None(NewLogWriter(buffer))
+	a.EnableEvent(EventDebug)
+	a.Writer().SetShowTimestamp(false)
+	a.Writer().SetShowLabel(false)
+	a.Writer().SetUseAnsiColors(false)
+	a.Sync().Infof("this is a %s", "test")
+	a.Sync().Debugf("this is a %s", "test")
+	assert.Equal("debug this is a test\n", buffer.String())
+}
